5cd491068a78051c0fea356bfd6f052ca92fe30d
Jean-Francois Lavoie <jflavoie.perso@gmail.com>
2022-12-19 11:40:20 -0500

    satisfy linter on nvim_install.sh

--- nvim_install.sh
+++ nvim_install.sh
@@ -1,47 +1,48 @@
 #!/bin/bash
 
 source .env.nvim
-currentFolder=$PWD
+currentFolder="$PWD"
 
-rm -rf $INSTALL_VI_ROOTPATH
+rm -rf "$INSTALL_VI_ROOTPATH"
 
-if [ ! -d $INSTALL_VI_ROOTPATH ]; then
-	mkdir -p $INSTALL_VI_ROOTPATH
+if [ ! -d "$INSTALL_VI_ROOTPATH" ]; then
+	mkdir -p "$INSTALL_VI_ROOTPATH"
 fi
 
-if [ -f $INSTALL_NVIM_APPIMAGE_PATH/nvim.appimage ]; then
-	rm $INSTALL_NVIM_APPIMAGE_PATH/nvim.appimage
+if [ -f "$INSTALL_NVIM_APPIMAGE_PATH"/nvim.appimage ]; then
+	rm "$INSTALL_NVIM_APPIMAGE_PATH"/nvim.appimage
 fi
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 
-mv nvim.appimage $INSTALL_NVIM_APPIMAGE_PATH
+mv nvim.appimage "$INSTALL_NVIM_APPIMAGE_PATH"
 
-ln -vfs $INSTALL_NVIM_APPIMAGE_PATH/nvim.appimage $INSTALL_NVIM_APPIMAGE_PATH/nvim
+ln -vfs "$INSTALL_NVIM_APPIMAGE_PATH"/nvim.appimage "$INSTALL_NVIM_APPIMAGE_PATH"/nvim
 
 function vimrc {
 
-	local dest=$currentFolder
+	local dest="$currentFolder"
 
-	pushd $INSTALL_VI_ROOTPATH
+	pushd "$INSTALL_VI_ROOTPATH" || exit
 
 	echo Symlinking init.vim
-	ln -sf $dest/init.vim init.vim
+	ln -sf "$dest"/init.vim init.vim
 
 	echo Symlinking init.lua
-	ln -sf $dest/init.lua init.vim.lua
+	ln -sf "$dest"/init.lua init.vim.lua
 
 	# lua files
 	mkdir -p lua/jf
-	pushd lua/jf
-	for f in $(ls $dest/lua/jf); do
-		echo Symlinking lua/$f
-		ln -sfv $dest/lua/jf/$f $f
+	pushd lua/jf || exit
+	# shellcheck disable=2045
+	for f in $(ls "$dest"/lua/jf); do
+		echo Symlinking lua/"$f"
+		ln -sfv "$dest"/lua/jf/"$f" "$f"
 	done
-	popd # lua/jf
+	popd || exit # lua/jf
 
-	popd # $INSTALL_VI_ROOTPATH
+	popd || exit # $INSTALL_VI_ROOTPATH
 }
 
 vimrc
