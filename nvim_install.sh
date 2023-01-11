bbe6e360972b10711b59665b1954a73d8b2d16b4
Jean-Francois Lavoie <jflavoie.perso@gmail.com>
2023-01-11 16:52:03 -0500

    housecleaning. comment code as a safety measure (and lazyness))

--- nvim_install.sh
+++ nvim_install.sh
@@ -3,68 +3,68 @@
 source .env.nvim
 currentFolder="$PWD"
 
-# rm -rf "$INSTALL_VI_ROOTPATH"
+rm -rf "$INSTALL_VI_ROOTPATH"
 
-# if [ ! -d "$INSTALL_VI_ROOTPATH" ]; then
-# 	mkdir -p "$INSTALL_VI_ROOTPATH"
-# fi
+if [ ! -d "$INSTALL_VI_ROOTPATH" ]; then
+	mkdir -p "$INSTALL_VI_ROOTPATH"
+fi
 
-# if [ -f "$INSTALL_NVIM_APPIMAGE_PATH"/nvim.appimage ]; then
-# 	rm "$INSTALL_NVIM_APPIMAGE_PATH"/nvim.appimage
-# fi
+if [ -f "$INSTALL_NVIM_APPIMAGE_PATH"/nvim.appimage ]; then
+	rm "$INSTALL_NVIM_APPIMAGE_PATH"/nvim.appimage
+fi
 
-# curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-# chmod u+x nvim.appimage
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
 
-# mv nvim.appimage "$INSTALL_NVIM_APPIMAGE_PATH"
-# chmod 755 "$INSTALL_NVIM_APPIMAGE_PATH"/nvim.appimage
+mv nvim.appimage "$INSTALL_NVIM_APPIMAGE_PATH"
+chmod 755 "$INSTALL_NVIM_APPIMAGE_PATH"/nvim.appimage
 
-# sudo ln -sfv "$INSTALL_NVIM_APPIMAGE_PATH"/nvim.appimage /usr/bin/nvim
+sudo ln -sfv "$INSTALL_NVIM_APPIMAGE_PATH"/nvim.appimage /usr/bin/nvim
 
-function links {
+# function links {
 
-	local dest="$currentFolder"
+# 	local dest="$currentFolder"
 
-	pushd "$INSTALL_VI_ROOTPATH" || exit
+# 	pushd "$INSTALL_VI_ROOTPATH" || exit
 
-	# echo Symlinking init.vim
-	# ln -sf "$dest"/init.vim init.vim
+# 	# echo Symlinking init.vim
+# 	# ln -sf "$dest"/init.vim init.vim
 
-	echo Symlinking init.lua
-	ln -sf "$dest"/init.lua init.lua
+# 	echo Symlinking init.lua
+# 	ln -sf "$dest"/init.lua init.lua
 
-	echo Symlinking custom snippets
-	if [ ! -L vim-jfsnippets ]; then
-		echo link did not exists
-		ln -sfv "$dest"/vim-jfsnippets vim-jfsnippets
-	fi
+# 	echo Symlinking custom snippets
+# 	if [ ! -L vim-jfsnippets ]; then
+# 		echo link did not exists
+# 		ln -sfv "$dest"/vim-jfsnippets vim-jfsnippets
+# 	fi
 
-	# lua files
-	mkdir -p lua/jf
-	pushd lua/jf || exit
-	# shellcheck disable=2045
-	for f in $(ls "$dest"/lua/jf); do
-		echo Symlinking lua/"$f"
-		ln -sfv "$dest"/lua/jf/"$f" "$f"
-	done
-	popd || exit # lua/jf
+# 	# lua files
+# 	mkdir -p lua/jf
+# 	pushd lua/jf || exit
+# 	# shellcheck disable=2045
+# 	for f in $(ls "$dest"/lua/jf); do
+# 		echo Symlinking lua/"$f"
+# 		ln -sfv "$dest"/lua/jf/"$f" "$f"
+# 	done
+# 	popd || exit # lua/jf
 
-	popd || exit # $INSTALL_VI_ROOTPATH
-}
+# 	popd || exit # $INSTALL_VI_ROOTPATH
+# }
 
-links
+# links
 
-# python3 -m pip install --upgrade virtualenv
-# sudo apt install python3.10-venv # required by Mason when installing debugpy
+python3 -m pip install --upgrade virtualenv
+sudo apt install python3.10-venv # required by Mason when installing debugpy
 
-# mkdir -p "$HOME/.virtualenvs"
-# pushd "$HOME"/.virtualenvs || exit
-# rm -rf pynvim
-# python3 -m venv pynvim
-# ./pynvim/bin/python3 -m pip install --upgrade pynvim
-# popd || exit # $HOME/.virtualenvs
+mkdir -p "$HOME/.virtualenvs"
+pushd "$HOME"/.virtualenvs || exit
+rm -rf pynvim
+python3 -m venv pynvim
+./pynvim/bin/python3 -m pip install --upgrade pynvim
+popd || exit # $HOME/.virtualenvs
 
-# # taken from :checkhealth
-# npm install -g neovim
+# taken from :checkhealth
+npm install -g neovim
 
-# ./plugins.sh
+./plugins.sh
